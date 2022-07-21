fe4db1515492dea77de4618dd7250e902592e772
rosibabubandi <rosibabuiiitn548@gmail.com>
2022-07-21 19:04:37 +0530

    changes

--- src/components/Careers/index.js
+++ src/components/Careers/index.js
@@ -7,45 +7,49 @@ const Careers = (props) => {
     <>
       <div>
         <p className="careers-text">CAREERS</p>
-        <h1 className="join-our-team-text">
-          Join our <span className="middle-word-in-getting">team</span>
-        </h1>
-        <hr className="hr-separator" />
-        <div>
-          <h1>Benefits</h1>
-          <button type="button" className="plus-cancel-button">
-            <img
-              src={plusSymbol}
-              className="plus-cancel-image-size"
-              alt="plus"
-            />
-          </button>
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <div style={{ width: "50%", padding: 50 }}>
+            <h1 className="join-our-team-text">
+              Join our <span className="middle-word-in-getting">team</span>
+            </h1>
+            <hr className="hr-separator" />
+            <div>
+              <h1>Benefits</h1>
+              <button type="button" className="plus-cancel-button">
+                <img
+                  src={plusSymbol}
+                  className="plus-cancel-image-size"
+                  alt="plus"
+                />
+              </button>
+            </div>
+            <hr className="hr-separator" />
+            <div>
+              <h1>Interview Process</h1>
+              <button type="button" className="plus-cancel-button">
+                <img
+                  src={plusSymbol}
+                  className="plus-cancel-image-size"
+                  alt="plus"
+                />
+              </button>
+            </div>
+            <hr className="hr-separator" />
+            <div>
+              <h1>Opportunities</h1>
+              <button type="button" className="plus-cancel-button">
+                <img
+                  src={plusSymbol}
+                  className="plus-cancel-image-size"
+                  alt="plus"
+                />
+              </button>
+            </div>
+            <hr className="hr-separator" />
+          </div>
+          <img src={careersHand} className="hand-images" alt="hand" />
         </div>
-        <hr className="hr-separator" />
-        <div>
-          <h1>Interview Process</h1>
-          <button type="button" className="plus-cancel-button">
-            <img
-              src={plusSymbol}
-              className="plus-cancel-image-size"
-              alt="plus"
-            />
-          </button>
-        </div>
-        <hr className="hr-separator" />
-        <div>
-          <h1>Opportunities</h1>
-          <button type="button" className="plus-cancel-button">
-            <img
-              src={plusSymbol}
-              className="plus-cancel-image-size"
-              alt="plus"
-            />
-          </button>
-        </div>
-        <hr className="hr-separator" />
       </div>
-      <img src={careersHand} className="hand-images" alt="hand" />
     </>
   );
 };
